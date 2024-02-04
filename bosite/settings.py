7665f21f2ad8d35d66f29375e3046aad8d9069f1
BoDai <dbo837035@gmail.com>
2024-02-05 10:31:36 +1100

    test simple ci/cd

--- bosite/settings.py
+++ bosite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,8 +25,7 @@ SECRET_KEY = "django-insecure-yb4#cxi&5upqq!3iq$7-ew1*9$7p=i6ul-_*=#@xp5e5)0m_=p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['3.106.250.180', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -122,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
